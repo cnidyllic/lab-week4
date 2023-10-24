7eed9bcc8958384a28ffc872af747b8ec95c1489
cnidyllic <146884284+cnidyllic@users.noreply.github.com>
2023-10-24 11:37:11 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,51 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+	import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests {
+
+    @Test 
+    public void testReverseInPlace_SingleElement() {
+        int[] input1 = { 3 };
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{ 3 }, input1);
+    }
+
+    @Test 
+    public void testReverseInPlace_MultipleElements() {
+        int[] input = { 1, 2, 3, 4, 5 };
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals(new int[]{ 5, 4, 3, 2, 1 }, input);
+    }
+
+    @Test 
+    public void testReverseInPlace_EvenNumberOfElements() {
+        int[] input = { 1, 2, 3, 4 };
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals(new int[]{ 4, 3, 2, 1 }, input);
+    }
+
+    @Test
+    public void testReversed_EmptyArray() {
+        int[] input1 = { };
+        assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    }
+
+    @Test
+    public void testReversed_MultipleElements() {
+        int[] input = { 1, 2, 3, 4, 5 };
+        int[] result = ArrayExamples.reversed(input);
+        assertArrayEquals(new int[]{ 5, 4, 3, 2, 1 }, result);
+    }
+
+    @Test
+    public void testReversed_EvenNumberOfElements() {
+        int[] input = { 1, 2, 3, 4 };
+        int[] result = ArrayExamples.reversed(input);
+        assertArrayEquals(new int[]{ 4, 3, 2, 1 }, result);
+    }
+}
+
 }
